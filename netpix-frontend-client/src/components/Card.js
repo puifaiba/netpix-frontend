653a3d28808e634cbae308755c4746669b326a2d
puifaiba <puifai.ba@gmail.com>
2021-03-30 15:55:24 -0400

    added react carousel

--- netpix-frontend-client/src/components/Card.js
+++ netpix-frontend-client/src/components/Card.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react'
-import "./card.scss";
+import React, {Component, useState} from "react"
+import "./card.scss"
+import ItemsCarousel from "react-items-carousel"
 
-export default class Card extends Component {
-    render() {
-        console.log("I got called");
-        const { title, overview, runtime, release_date, src, image_src } = this.props.movie
-        return (
-            <a className="carousel-item"><img src={image_src} weight="200" height="120" /></a>
-            // <div className="item">
-            //     <img src={image_src} alt={`Image of ${title}`} />
-            //     <h2>{title}</h2>
-            // </div>
-            // <div >
-            //     <div className="bg-img">
-            //     </div>
-            //     <a href="#">
-            //         <div className="content">
-            //             <h2>{title}</h2>
-            //         </div>
-            //     </a>
-            // </div>
-        )
-    }
+function Card({title, overview, runtime, release_date, src, image_src}) {
+  const [activeItemIndex, setActiveItemIndex] = useState(0)
+  const chevronWidth = 40
+  return (
+    <div style={{padding: 0, maxWidth: "100%", margin: "0"}}>
+      <ItemsCarousel
+        infiniteLoop={false}
+        gutter={12}
+        activePosition={"center"}
+        chevronWidth={60}
+        disableSwipe={false}
+        alwaysShowChevrons={false}
+        numberOfCards={4}
+        slidesToScroll={3}
+        outsideChevron={false}
+        showSlither={false}
+        firstAndLastGutter={true}
+        activeItemIndex={activeItemIndex}
+        requestToChangeActive={(value) => setActiveItemIndex(value)}
+        rightChevron={">"}
+        leftChevron={"<"}
+      >
+        <div style={{height: 200, background: "#EEE"}}>First card</div>
+        <div style={{height: 200, background: "#EEE"}}>Second card</div>
+        <div style={{height: 200, background: "#EEE"}}>Third card</div>
+        <div style={{height: 200, background: "#EEE"}}>Fourth card</div>
+        <div style={{height: 200, background: "#000"}}>Fifth card</div>
+        <div style={{height: 200, background: "#AAA"}}>Sixth card</div>
+        {/* {Array.from(new Array(10)).map((_, i) => (
+          <div
+            key={i}
+            style={{
+              height: 200,
+              background: "url(https://placeimg.com/380/200/nature)",
+            }}
+          /> */}
+        ))}
+      </ItemsCarousel>
+    </div>
+    // <a className="carousel-item"><img src={image_src} weight="200" height="120" /></a>
+    // <div className="item">
+    //     <img src={image_src} alt={`Image of ${title}`} />
+    //     <h2>{title}</h2>
+    // </div>
+    // <div >
+    //     <div className="bg-img">
+    //     </div>
+    //     <a href="#">
+    //         <div className="content">
+    //             <h2>{title}</h2>
+    //         </div>
+    //     </a>
+    // </div>
+  )
 }
+
+export default Card
--- netpix-frontend-client/src/containers/Home.js
+++ netpix-frontend-client/src/containers/Home.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react'
-import Card from "../components/Card";
-import "../components/card.scss";
-import M from "materialize-css";
+import React, {Component} from "react"
+import Card from "../components/Card"
+import "../components/card.scss"
+// import M from "materialize-css";
 
 const BASE_URL = "http://localhost:3000/"
 const GET_MOVIES = `${BASE_URL}movies/`
 
-
 export default class Home extends Component {
-    state = {
-        movies: []
-    }
+  state = {
+    movies: [],
+  }
 
-    // fetchData = async () => {
-    //     const res = await fetch(GET_MOVIES)
-    //     const movies = await res.json()
-    //     return movies
-    // }
+  // fetchData = async () => {
+  //     const res = await fetch(GET_MOVIES)
+  //     const movies = await res.json()
+  //     return movies
+  // }
 
-    componentDidMount = () => {
-        fetch(GET_MOVIES).then(res => res.json()).then(movies => this.setState({ movies }))
-        let elems = document.querySelectorAll('.carousel');
-        M.Carousel.init(elems, { duration: 200 });
-    }
+  componentDidMount = () => {
+    fetch(GET_MOVIES)
+      .then((res) => res.json())
+      .then((movies) => this.setState({movies}))
+    // let elems = document.querySelectorAll('.carousel');
+    // M.Carousel.init(elems, { duration: 200 });
+  }
 
-    render = () => {
-        return (
-            <div>
-                {/* Highlight */}
-                {/* <div className="flix-container"> */}
-                <div className="carousel">
-                    {this.state.movies.map(movie => <Card movie={movie} />)}
-                </div>
-                {/* </div> */}
-
-            </div>
-        )
-    }
+  render = () => {
+    return (
+      <div>
+        {/* Highlight */}
+        {/* <div className="flix-container"> */}
+        <div className="carousel">
+          {this.state.movies.map((movie) => (
+            <Card movie={movie} />
+          ))}
+        </div>
+        {/* </div> */}
+      </div>
+    )
+  }
 }
